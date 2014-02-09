f151da5bfc01c3e87bb872b4bc479a8b14ce3edd
Wasteland Comm <n8mjk@wastecomm.net>
2014-02-09 15:24:17 -0800

    Adds use of several now standardized functions from kspmc.py and removes the old local definitions of the same.

--- fdo.py
+++ fdo.py
@@ -1,11 +1,11 @@
 #! /usr/bin/python
 # coding=UTF-8
-#--------------------
+#----------------------
 # KSP Telemachus
 # Mission Control
-# Flight Dynamics v0.6
+# Flight Dynamics v0.65
 # By Erik N8MJK
-#--------------------
+#----------------------
 
 import time
 import datetime
@@ -21,69 +21,18 @@ import random
 import pika
 import marshal
 import math
+from kspmc import *
 
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
-def xstr(s):
-    if s is None:
-        return ''
-    return str(s)
-
-def pfloat(num):
-    if isNum(num):
-        nnum = xstr("{:,}".format(round(num,2)))
-    else:
-        nnum = num
-    return nnum
-
-def get_datetime():
-    #let's make a pretty datetime
-    global timeoutput
-    global dateoutput
-    t = datetime.datetime.now()
-    currdatetime = t.timetuple()
-    dateoutput = time.strftime("%Y-%m-%d",currdatetime)
-    timeoutput = time.strftime("%d %b %Y %H:%M:%S",currdatetime)
-
-def write_datetime(win):
-    #separate function since this gets done A LOT
-    get_datetime()
-
-    win.move(0,59)
-    win.clrtoeol()
-    win.addstr(timeoutput, curses.A_REVERSE)
-    win.refresh()
-
 def init_window(win):
     win.erase()
-    topstring = "FLIGHT DYNAMICS v0.6"
+    topstring = "FLIGHT DYNAMICS v0.65"
     bottomstring = "ORBITAL PARAMETERS AND DESIRED CHANGES"
     bottomfillstring = (78- len(bottomstring)) * " "
     topfillstring  = (78 - len(topstring)) * " "
     win.addstr(0,0," " + topstring + topfillstring, curses.A_REVERSE)
     win.addstr(23,0,bottomfillstring + bottomstring + " ", curses.A_REVERSE)
-
-def isNum(num):
-    try:
-        float(num)
-        return True
-    except ValueError:
-        pass
-    return False
-
-def rSlop(num):
-    if isNum(num):
-        nnum = num * random.uniform(0.99,1.01)
-        return nnum
-    else:
-        return num
-
-def rAlt(num):
-    if isNum(num):
-        nnum = round(int(num),-2)
-        return nnum
-    else:
-        return num
 
 def getRadar(d):
     maxralt = 7000000 #max radar altitude
@@ -173,277 +122,6 @@ def getRadar(d):
         else:
             d["ralt"] = round(d["ralt"], -2)
     return d
-
-def getTelemetry(d):
-    maxgralt = "15000" #max altitude for ground radar
-    if d["ttt1"] > 0:
-        d["t1"] = d["mt"] + d["ttt1"]
-    else:
-        d["t1"] = d["ttt1"]
-    if d["ttt2"] > 0:
-        d["t2"] = d["mt"] + d["ttt2"]
-    else:
-        d["t2"] = d["ttt2"]
-    d["apat"] = d["mt"] + d["ttap"]
-    d["peat"] = d["mt"] + d["ttpe"]
-    if isNum(d["pe"]) and d["pe"] < 0:
-        d["pe"] = 0
-    if d["pstat"] == 0:
-        d["lat"] = rSlop(d["lat"])
-        d["long"] = rSlop(d["long"])
-    d["altt"] = "?"
-    if isNum(d["vs"]):
-        if d["vs"] < 0:
-            d["altt"] = "-"
-        else:
-            d["altt"] = "+"
-        if int(d["vs"]) == 0:
-            d["altt"] = " "
-        if isNum(d["vs"]):
-            d["hs"] = d["sfcs"] - abs(d["vs"])
-            d["vs"] = abs(d["vs"])
-        else:
-            d["hs"] = " "
-    d["asl"] = d["alt"]
-    if isNum(d["lf"]) and isNum(d["oxidizer"]):
-        d["fuel"] = d["lf"] + d["oxidizer"]
-    if isNum(d["mlf"]) and isNum(d["moxidizer"]):
-        d["mfuel"] = d["mlf"] + d["moxidizer"]
-    if isNum(d["asl"]):
-        if d["hat"] == -1 or d["hat"] > maxgralt or d["pstat"] != 0:
-            d["grstatus"] = "UNAVAIL"
-            d["hat"] = "MAX"
-            d["asl"] = " "
-            d["vs"] = " "
-            d["hs"] = " "
-            d["sfcv"] = " "
-            d["sfcvx"] = " "
-            d["sfcvy"] = " "
-            d["sfcvz"] = " "
-        else:
-            d["grstatus"] = "NOMINAL"
-        return d
-
-def fuck(status,instring):
-    if status == 0 or status == 1:
-        return instring
-    if isNum(instring):
-        workstring = str(instring)
-    else:
-        workstring = instring
-    worklist = list(workstring)
-    if status == 2:
-        for i,char in enumerate(worklist):
-            charlist = [char,char,char,char,char,char,char,char,char,char,char,char,char,char,char,char,char,char,'!','?','i','$','/','|','#']
-            newchar = random.choice(charlist)
-            worklist[i] = newchar
-        outstring = "".join(worklist)
-    if status == 3 or status == 4:
-        for i,char in enumerate(worklist):
-            newchar = " "
-            worklist[i] = newchar
-        outstring = "".join(worklist)    
-    return outstring
-
-def fucknum(status,indata):
-    if status == 0 or status == 1:
-        return indata
-    if status == 2:
-        if isNum(indata):
-            errnum = random.uniform(0.75,1.25)
-            outdata = indata * errnum
-        else:
-            return indata
-    if status == 3 or status == 4:
-        outdata = 0
-    return outdata
-
-def pnum(num):
-    if isNum(num):
-        nnum = xstr("{:,}".format(int(num)))
-    else:
-        nnum = num
-    return nnum
-
-def pvel(num):
-    kmlimit = 10000
-    if isNum(num):
-        if num < kmlimit:
-            nnum = xstr("{:,}".format(int(round(num,0)))) + "m/s"
-        if num >= kmlimit:
-            nnum = xstr("{:,}".format(round(num / 1000,1))) + "km/s"
-    else:
-        nnum = num
-    return nnum
-
-def phbar(num,mnum):
-    if isNum(num) and isNum(mnum):
-        pnum = int((num / mnum) * 100)
-        onum = xstr(" " + "{:,}".format(round(num,1))) + " (" + xstr(pnum) + "%)"
-    else:
-        onum = num
-    return onum
-
-def printwarn(win,warn,state):
-    if state == 0:
-        win.addstr(1,1,warn,curses.A_BOLD)
-    else:
-        win.addstr(1,1,warn,curses.A_BLINK + curses.A_REVERSE)
-
-def printhbar(win,instr,perc):
-    i = 0
-    barperc = int(35 * perc)
-    barstring = instr.ljust(35)
-    while i < 35:
-        if i < barperc:
-            win.addstr(barstring[i],curses.A_REVERSE)
-        else:
-            win.addstr(barstring[i])
-        i = i + 1
-
-def printvbar(win,perc):
-    i = 0
-    output = format(xstr(int(perc * 100)),">3s")
-    barperc = int(9 * perc)
-    while i < 9:
-        if i < barperc:
-            win.addstr(9-i,1,output,curses.A_REVERSE)
-        else:
-            win.addstr(9-i,1,output)
-        i = i + 1
-        output = "   "
-
-def printvdef(win,perc):
-    i = 0
-    output = format(xstr(int(perc * 100)),">3s")
-    barperc = int(9 * perc)
-    while i < 9:
-        if i < barperc:
-            win.addstr(9-i,1,output,curses.A_REVERSE)
-        else:
-            win.addstr(9-i,1,output)
-        i = i + 1
-        output = "   "
-   
-def pdeg(inum):
-    if isNum(inum):
-        num = xstr(abs(int(inum))).zfill(3)
-        if inum < 0:
-            nnum = "-%s" % num
-        else:
-            nnum = "+%s" % num
-    else:
-        nnum = inum
-    return nnum
-
-def ptime(num):
-    if isNum(num):
-        m, s = divmod(num, 60)
-        h, m = divmod(m, 60)
-        h = str(int(h)).zfill(2)
-        m = str(int(m)).zfill(2)
-        s = str(int(s)).zfill(2)
-        nnum = "%s:%s:%s" % (h,m,s)
-    else:
-        nnum = num
-    return nnum
-
-def pltime(num):
-    if isNum(num):
-        if num >= 7884000:
-            m, s = divmod(num, 60)
-            h, m = divmod(m, 60)
-            d, h = divmod(h, 6)
-            y, d = divmod(d, 365)
-            y = xstr(int(y))
-            d = xstr(int(d)).zfill(2)
-            nnum = "%sy %sd" % (y,d)
-        else:
-            m, s = divmod(num, 60)
-            h, m = divmod(m, 60)
-            d, h = divmod(h, 6)
-            d = xstr(int(d))
-            h = xstr(int(h)).zfill(2)
-            m = xstr(int(m)).zfill(2)
-            s = xstr(int(s)).zfill(2)
-            nnum = "%sd%s:%s:%s" % (d,h,m,s)
-    else:
-        nnum = num
-    return nnum
-
-def palt(num):
-    kmlimit = 100000
-    mmlimit = 999999000
-    if isNum(num):
-        if num < kmlimit:
-            nnum = xstr("{:,}".format(int(num))) + "m"
-        if num >= kmlimit:
-            nnum = xstr("{:,}".format(int(num / 1000))) + "km"
-        if num >= mmlimit:
-            nnum = xstr("{:,}".format(int(num / 1000000))) + "Mm"
-    else:
-        nnum = num
-    return nnum
-
-def pwgt(num):
-    tonlimit = 10000
-    if isNum(num):
-        if num >= tonlimit:
-            nnum = xstr("{:,}".format(round(num / 1000,3))) + "t"
-        if num < tonlimit:
-            nnum = xstr("{:,}".format(int(round(num,0)))) + "kg"
-    else:
-        nnum = num
-    return nnum
-
-def plat(inum):
-    if isNum(inum):
-        num = abs(inum)
-        latmin = num - int(num)
-        latdeg = num - latmin
-        latmin = latmin * 60
-        min = xstr(int(latmin)).zfill(2) + "'"
-        deg = xstr(int(latdeg)).zfill(3) + " "
-        if num < 0:
-            nnum = deg + min + "S"
-        else:
-            nnum = deg + min + "N"
-    else:
-        nnum = inum
-    return nnum
-
-def plong(inum):
-    if isNum(inum):
-        if inum > 180:
-            num = inum - 360
-        else:
-            num = inum
-        longmin = abs(num - int(num))
-        longdeg = abs(num - longmin)
-        longmin = longmin * 60
-        min = xstr(int(longmin)).zfill(2) + "'"
-        deg = xstr(int(longdeg)).zfill(3) + " "
-        if num < 0:
-            nnum = deg + min + "W"
-        else:
-            nnum = deg + min + "E"
-    else:
-        nnum = inum
-    return nnum
-
-def init_time_window(win,y,x,title):
-    timewin = curses.newwin(2,14,y,x)
-    timewin.box()
-    timewin.bkgd(curses.color_pair(1));
-    win.refresh()
-    timewin.addstr(0,1,title,curses.A_BOLD)
-    timewin.refresh()
-    return timewin
-
-def draw_time_window(win,data):
-    win.addstr(1,1,"           ",curses.A_BOLD)
-    win.addstr(1,1,pltime(data),curses.A_BOLD)
-    win.refresh()
 
 def init_input_window(win,y,x,title):
     iwin = curses.newwin(2,9,y,x)
@@ -631,79 +309,6 @@ def draw_dv_window(win,data):
     win.addstr(5,8,ptime(btmax),curses.A_BOLD)
     win.addstr(7,8,fuck(pstat,palt(data["alt"])),curses.A_BOLD)
     win.addstr(8,8,palt(0),curses.A_BOLD)
-    win.refresh()
-
-def init_hbar_window(win,y,x,title):
-    fuelwin = curses.newwin(3,37,y,x)
-    fuelwin.box()
-    fuelwin.bkgd(curses.color_pair(1));
-    win.refresh()
-    fuelwin.addstr(0,1,title,curses.A_BOLD)
-    fuelwin.refresh()
-    return fuelwin
-
-def draw_hbar_window(win,data,key,mkey):
-    win.addstr(1,1,"                    ",curses.A_BOLD)
-    pstat = data["pstat"]
-    fkey = fucknum(pstat,data[key])
-    if isNum(mkey):
-        fmkey = mkey
-    else:
-        fmkey = data[mkey]
-    hbar = phbar(fkey,fmkey)
-    hperc = fkey / fmkey
-    win.move(1,1)
-    printhbar(win,hbar,hperc)
-    win.refresh()
-
-def init_alarm_window(win,y,x):
-    wgwin = curses.newwin(3,7,y,x)
-    wgwin.box()
-    wgwin.bkgd(curses.color_pair(1));
-    win.refresh()
-    wgwin.addstr(1,1,"     ",curses.A_BOLD)
-    wgwin.refresh()
-    return wgwin
-
-def draw_alarm_window(win,data):
-    lfp = data["lf"] / data["mlf"]
-    oxip = data["oxidizer"] / data["moxidizer"]
-    monop = data["mono"] / data["mmono"]
-    o2p = data["o2"] / data["mo2"]
-    h2op = data["h2o"] / data["mh2o"]
-    foodp = data["food"] / data["mfood"]
-    mp = data["w"] / data["mw"]
-    if o2p > 0.1 and h2op > 0.1 and foodp > 0.1:
-        lsstatus = 0
-    else:
-        lsstatus = 1
-    if lfp > 0.1 and oxip > 0.1 and monop > 0.1 and lsstatus == 0 and mp > 0.1:
-        state = 0
-    else:
-        state = 1
-    printwarn(win,"ALARM",state)
-    win.refresh()
-
-def init_throt_window(win,y,x):
-    pitchwin = curses.newwin(11,5,y,x)
-    pitchwin.box()
-    pitchwin.bkgd(curses.color_pair(1));
-    win.refresh()
-    pitchwin.addstr(1,0,"T",curses.A_BOLD)
-    pitchwin.addstr(2,0,"H",curses.A_BOLD)
-    pitchwin.addstr(3,0,"R",curses.A_BOLD)
-    pitchwin.addstr(4,0,"U",curses.A_BOLD)
-    pitchwin.addstr(5,0,"S",curses.A_BOLD)
-    pitchwin.addstr(6,0,"T",curses.A_BOLD)
-    pitchwin.addstr(9,4,"%",curses.A_BOLD)
-    pitchwin.refresh()
-    return pitchwin
-
-def draw_throt_window(win,data):
-    win.move(1,1)
-    pstat = data["pstat"]
-    throt = fucknum(pstat,data["throt"])
-    printvbar(win,throt)
     win.refresh()
 
 def mainloop(win):
